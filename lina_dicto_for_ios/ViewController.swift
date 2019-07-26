2711f52c771a070f877d6dde0ef90af72e432394
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2019-07-26 21:58:36 +0900

    Initial Commit

---
+++ lina_dicto_for_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lina_dicto_for_ios
+//
+//  Created by Michinari Nukazawa on 2019/07/26.
+//  Copyright © 2019 Michinari Nukazawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lina_dicto_for_iosTests/lina_dicto_for_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  lina_dicto_for_iosTests.swift
+//  lina_dicto_for_iosTests
+//
+//  Created by Michinari Nukazawa on 2019/07/26.
+//  Copyright © 2019 Michinari Nukazawa. All rights reserved.
+//
+
+import XCTest
+@testable import lina_dicto_for_ios
+
+class lina_dicto_for_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
