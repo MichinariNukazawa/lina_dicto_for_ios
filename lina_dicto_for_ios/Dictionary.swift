b6b6666808172d80953c4f761f38907263fa8ca7
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2019-07-27 01:46:25 +0900

    storyboard

---
+++ lina_dicto_for_ios/Dictionary.swift
@@ -0,0 +1,9 @@
+//
+//  Dictionary.swift
+//  lina_dicto_for_ios
+//
+//  Created by Michinari Nukazawa on 2019/07/26.
+//  Copyright © 2019 Michinari Nukazawa. All rights reserved.
+//
+
+import Foundation
